3d82506d3632877ee32b7383c20d945d3f295610
isamyakt <isamyakt17@gmail.com>
2023-06-17 17:56:29 +0530

    mining block and generating nonce for given data(tx) 🚀

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,15 @@
 use rust_blockchain::{block::Block, hash_function::HashFunction};
 
 fn main() {
-    let mut block = Block::new(0, 0, vec![0; 32], 0, "genesis block!".to_string());
+    let mut block = Block::new(0, 0, vec![0; 32], 0, "genesis block!".to_string(), 0x000fffffffffffffffffffffffffffff);
+
+    block.hash = block.hash();
 
     println!("{:?}", &block);
 
-    let hash = block.hash();
+    block.mine();
 
-    println!("{:?}", &hash);
+    println!("{:?}", &block);
 
-    block.hash = hash;
-
-    println!("{:?}", block);
+    
 }
