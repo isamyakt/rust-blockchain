39f486b69ec1373af13ea957a118345890972a06
isamyakt <isamyakt17@gmail.com>
2023-06-17 17:50:49 +0530

    difficulty u128 bytes ⏳

--- src/lib.rs
+++ src/lib.rs
@@ -47,5 +47,25 @@ pub fn u128_bytes (u: &u128) -> [u8; 16] {
     ]
 }
 
+pub fn difficulty_bytes_as_u128 (hash: &Vec<u8>) -> u128 {
+    ((hash[31] as u128) << 0xf * 8) |
+    ((hash[30] as u128) << 0xe * 8) |
+    ((hash[29] as u128) << 0xd * 8) |
+    ((hash[28] as u128) << 0xc * 8) |
+    ((hash[27] as u128) << 0xb * 8) |
+    ((hash[26] as u128) << 0xa * 8) |
+    ((hash[25] as u128) << 0x9 * 8) |
+    ((hash[24] as u128) << 0x8 * 8) |
+    ((hash[23] as u128) << 0x7 * 8) |
+    ((hash[22] as u128) << 0x6 * 8) |
+    ((hash[21] as u128) << 0x5 * 8) |
+    ((hash[20] as u128) << 0x4 * 8) |
+    ((hash[19] as u128) << 0x3 * 8) |
+    ((hash[18] as u128) << 0x2 * 8) |
+    ((hash[17] as u128) << 0x1 * 8) |
+    ((hash[16] as u128) << 0x0 * 8)
+}
+
+
 pub mod block;
 pub mod hash_function;
