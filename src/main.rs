dab5b12aba8e7b934f488bf59c0ef479b9f87d7c
isamyakt <isamyakt17@gmail.com>
2023-06-18 09:33:58 +0530

    running blockchain 🚀

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,44 @@
-use rust_blockchain::{block::Block, hash_function::HashFunction};
+use rust_blockchain::{
+    block::Block, 
+    hash_function::HashFunction, 
+    blockchain::Blockchain, 
+    time_now
+};
 
 fn main() {
-    let mut block = Block::new(0, 0, vec![0; 32], 0, "genesis block!".to_string(), 0x000fffffffffffffffffffffffffffff);
-
-    block.hash = block.hash();
-
-    println!("{:?}", &block);
+    let difficulty = 0x000fffffffffffffffffffffffffffff;
+    let mut block = Block::new(0, time_now(), vec![0; 32], 0, "genesis block!".to_string(), difficulty);
 
     block.mine();
 
-    println!("{:?}", &block);
+    println!("Genesis block: {:?}", &block);
 
-    
+    let mut last_hash = block.hash().clone();
+
+    let mut blockchain = Blockchain {
+        blocks: vec![block],
+    };
+
+    println!("Verify: {}", &blockchain.verify());
+
+    for i in 1..=10 {
+        let mut block = Block::new(i, time_now(), last_hash, 0, "next block".to_string(), difficulty);
+
+        block.mine();
+        println!("Mined block: {:?}", &block);
+
+        last_hash = block.hash.clone();
+
+        blockchain.blocks.push(block);
+        
+        println!("Verify: {}", &blockchain.verify());
+    }
+
+        // ERRORS
+    // blockchain.blocks[4].index = 5;   // Index mismatch
+    // blockchain.blocks[7].hash[7] += 1;   // Hash mismatch
+    // blockchain.blocks[8].data = "data changed".to_string();    // data mismatch
+    // blockchain.blocks[9].prev_block_hash[17] = 8;  // difficulty fail
+    // println!("Verify: {}", &blockchain.verify());
+
 }
